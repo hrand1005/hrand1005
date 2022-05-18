eec4a20eeecb40724edeb2a0994b65f9273c3d3c
Herbie <hjrand1005@gmail.com>
2022-05-18 19:01:43 -0400

    ADD: build essential install step

--- setup.sh
+++ setup.sh
@@ -26,3 +26,6 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin">>$HOME/.profile
 source $HOME/.profile
 rm go1.18.2.linux-amd64.tar.gz
+
+echo "\nInstalling build-essential"
+apt install --reinstall build-essential
